73a1bfcd999160882840d8430480797e38cfa400
davidan <pepe@arqueta.es>
2021-11-02 09:40:33 +0100

    Inicializado ejercicios js
    -Creado js. Realizados ejercicios para alumnos

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict"
+//Ejercicio 1
+
+let nombrePila = "David"
+console.log (nombrePila)
+
+//Ejercicio 2
+
+nombrePila = "David"
+console.log(nombrePila)
+
+//Ejercicio 3
+//si comento variable dice que no está definida
+
+let operador1=3;
+let operador2=5, operador3="David";
+let resultado=0;
+
+resultado = operador1+operador2;
+
+
+console.log(resultado)
